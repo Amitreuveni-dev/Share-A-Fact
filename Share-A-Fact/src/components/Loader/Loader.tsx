e08fa1ced971db8ca89b0c5d48d336e34035219d
amit reuveni <amitr123r@gmail.com>
2026-03-09 15:19:25 +0200

    organized the repo

--- Share-A-Fact/src/components/Loader/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Loader.module.scss";
-
-function Loader() {
-  return <p className={styles.message}>Loading...</p>;
-}
-
-export default Loader;
--- Share-A-Fact/src/services/supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://eyqyciyiehvnkqwptlve.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5cXljaXlpZWh2bmtxd3B0bHZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzOTk5ODIsImV4cCI6MjA3ODk3NTk4Mn0.Y3T7n-7QmA6aL-xoWCZEcX79L9S-Rjq-KyRilMTxx6E";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
